5fc029526492bc4da13974a0a3860f4b35a1240a
Kamil Baczynski <kamil-baczynski1@wp.pl>
2020-04-07 21:15:06 +0200

    Feat: Parallax effect on background image in hero section

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,37 @@
-"use strict";
+// "use strict";
 
-const switcherNav = document.querySelector('.top-nav__switcher--js');
+// const switcherNav = document.querySelector('.top-nav__switcher--js');
 
-switcherNav.addEventListener('click', (e) => {
-  const navigationList = document.querySelector('.top-nav__list--js');
-  navigationList.classList.toggle('top-nav__list--visible');
+// switcherNav.addEventListener('click', (e) => {
+//   const navigationList = document.querySelector('.top-nav__list--js');
+//   navigationList.classList.toggle('top-nav__list--visible');
 
-  const navigationBar = document.querySelector('.top-nav--js');
-  navigationBar.classList.toggle('top-nav--visible');
+//   const navigationBar = document.querySelector('.top-nav--js');
+//   navigationBar.classList.toggle('top-nav--visible');
 
-  if (navigationList.classList.contains('top-nav__list--visible')) {
-      switcher.innerHTML = 'X';
-  }
-  else {
-      switcher.innerHTML = '☰';
-  }
+//   if (navigationList.classList.contains('top-nav__list--visible')) {
+//       switcher.innerHTML = 'X';
+//   }
+//   else {
+//       switcher.innerHTML = '☰';
+//   }
+// });
+
+// const switcherSide = document.querySelector('.side-nav__button-tel--js');
+
+// switcherSide.addEventListener('click', (e) => {
+//   const navigationTel = document.querySelector('.side-nav__button-tel--link');
+//   navigationTel.classList.toggle('side-nav__button-tel--link-visible');
+
+//   const navigationBut = document.querySelector('.side-nav__button-tel--js')
+//   navigationBut.classList.toggle('side-nav__button-tel--visible')
+// });
+
+
+//Hero section parallax effect
+window.addEventListener('scroll', e => {
+  const parallax = document.querySelector('.parallax');
+  let posY = window.pageYOffset * 0.2;
+
+  parallax.style.transform = `translateY(-${posY}px)`;
 });
-
-const switcherSide = document.querySelector('.side-nav__button-tel--js');
-
-switcherSide.addEventListener('click', (e) => {
-  const navigationTel = document.querySelector('.side-nav__button-tel--link');
-  navigationTel.classList.toggle('side-nav__button-tel--link-visible');
-
-  const navigationBut = document.querySelector('.side-nav__button-tel--js')
-  navigationBut.classList.toggle('side-nav__button-tel--visible')
-});
-
-
